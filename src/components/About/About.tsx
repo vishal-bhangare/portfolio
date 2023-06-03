f8cae91c7b4b815fa01889118f7d1b570eaa57d5
vishalbhangare <vishalbhangare2@gmail.com>
2023-06-03 21:16:31 +0530

    skills and links added

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,8 @@
 import Media from "react-media";
 import styles from "./About.module.scss";
+import { socialMediaLinks, otherProfiles } from "../../data/links";
+import { skills } from "../../data/skills";
+import parse from "html-react-parser";
 
 const About = () => {
   return (
@@ -35,45 +38,53 @@ const About = () => {
               <span className={styles.arrow}>{">"}</span>
               <span className={styles.linksTitle}>Follow me on :</span>
               <div className={styles.icons}>
-                <a href="" target="_blank">
-                  <i className="fa-brands fa-facebook"></i>
-                </a>
-                <a href="" target="_blank">
-                  <i className="fa-brands fa-twitter"></i>
-                </a>
-                <a href="" target="_blank">
-                  <i className="fa-brands fa-linkedin"></i>
-                </a>
+                {socialMediaLinks.map((link) => (
+                  <a href={link.url} target="_blank">
+                    {parse(link.icon)}
+                  </a>
+                ))}
               </div>
             </div>
             <div className={[styles.otherProfiles, styles.links].join(" ")}>
               <span className={styles.arrow}>{">"}</span>
               <span className={styles.linksTitle}>Some other profiles :</span>
               <div className={styles.icons}>
-                <a href="" target="_blank">
-                  <i className="fa-brands fa-github"></i>
-                </a>
-                <a href="" target="_blank">
-                  {" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    enable-background="new 0 0 24 24"
-                    viewBox="0 0 24 24"
-                    id="leetcode"
-                  >
-                    <path
-                      fill="#fff"
-                      d="M22,14.355c0-0.742-0.564-1.346-1.26-1.346H10.676c-0.696,0-1.26,0.604-1.26,1.346s0.563,1.346,1.26,1.346H20.74C21.436,15.702,22,15.098,22,14.355z"
-                    ></path>
-                    <path
-                      fill="#fff"
-                      d="M3.482,18.187l4.313,4.361C8.768,23.527,10.113,24,11.598,24c1.485,0,2.83-0.512,3.805-1.494l2.588-2.637c0.51-0.514,0.492-1.365-0.039-1.9c-0.531-0.535-1.375-0.553-1.884-0.039l-2.676,2.607c-0.462,0.467-1.102,0.662-1.809,0.662s-1.346-0.195-1.81-0.662l-4.298-4.363c-0.463-0.467-0.696-1.15-0.696-1.863c0-0.713,0.233-1.357,0.696-1.824l4.285-4.38c0.463-0.467,1.116-0.645,1.822-0.645s1.346,0.195,1.809,0.662l2.676,2.606c0.51,0.515,1.354,0.497,1.885-0.038c0.531-0.536,0.549-1.387,0.039-1.901l-2.588-2.636c-0.649-0.646-1.471-1.116-2.392-1.33l-0.034-0.007l2.447-2.503c0.512-0.514,0.494-1.366-0.037-1.901c-0.531-0.535-1.376-0.552-1.887-0.038L3.482,10.476C2.509,11.458,2,12.813,2,14.311C2,15.809,2.509,17.207,3.482,18.187z"
-                    ></path>
-                  </svg>
-                </a>
+                {otherProfiles.map((link) => (
+                  <a href={link.url} target="_blank">
+                    {parse(link.icon)}
+                  </a>
+                ))}
               </div>
-
-              {/* <i className="fab fa-hackerrank"></i> */}
+            </div>
+          </div>
+          <div className={styles.skillsContainer}>
+            <div className={styles.subtitle}>Skills :</div>
+            <div className={styles.Content}>
+              {skills.map((skill) => (
+                <div className={styles.skills}>
+                  <div className={styles.CategoryName}>
+                    {skill.category} skills
+                  </div>
+                  <div className={styles.CategorySkills}>
+                    {skill.skills.map((s) => (
+                      <div className={styles.skill}>{s.name}</div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+              {/* <div className={styles.skills}>
+                <div className={styles.CategoryName}>Core Skills</div>
+                <div className={styles.CategorySkills}>
+                  <div className={styles.skill}>C++</div>
+                  <div className={styles.skill}>C</div>
+                  <div className={styles.skill}>Java</div>
+                </div>
+              </div> */}
+              {/* {socialMediaLinks.map((link) => (
+                <a href={link.url} target="_blank">
+                  {parse(link.icon)}
+                </a>
+              ))} */}
             </div>
           </div>
         </div>
---
+++ src/data/skills.ts
@@ -0,0 +1,41 @@
+export const skills = [
+  {
+    category: "Core",
+    skills: [
+      { name: "C", level: 30 },
+      { name: "C++", level: 40 },
+      { name: "Java", level: 60 },
+      { name: "Python", level: 60 },
+      { name: "DSA", level: 50 },
+    ],
+  },
+  {
+    category: "Frontend",
+    skills: [
+      { name: "HTML", level: 85 },
+      { name: "CSS", level: 60 },
+      { name: "SCSS", level: 40 },
+      { name: "Angular", level: 40 },
+      { name: "React", level: 20 },
+      { name: "Javascript", level: 60 },
+      { name: "Typescript", level: 40 },
+    ],
+  },
+  {
+    category: "Backend",
+    skills: [
+      { name: "NodeJS", level: 30 },
+      { name: "Express", level: 40 },
+    ],
+  },
+  {
+    category: "Database",
+    skills: [
+      { name: "SQL", level: 30 },
+      { name: "PlSQL", level: 40 },
+      { name: "MySQL", level: 60 },
+      { name: "MongoDb", level: 50 },
+      { name: "OracleDb", level: 70 },
+    ],
+  },
+];
