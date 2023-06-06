6513971f1cbf4e4c35af734221de5339cf579cd1
vishalbhangare <vishalbhangare2@gmail.com>
2023-06-06 08:25:55 +0530

    patch 0.0.1

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ export const Projects = [
     description: "I have created this notes app using <u>Java</u>",
     date: "feb 2023",
     image:
-      "https://uc27fbbe1c22ba044cb0a27ce964.previews.dropboxusercontent.com/p/thumb/AB7KjhfsXEXbzUD6VbBTL7uz7Aqxw3EFmBMEfiBBNHghkiBLw_r_Q6rYq4syzmtcZ5UU6Ymm2rqiA2ohsFVSYLvs34e3Yo3OvzDwBvLI8YF4hLt7zGSBIR9OBvlqq75NYc-1xnUXR7-1dVycGQRqZly5kR2CNa-pVNDromcHfB085SsldEXabvVkLcHG-ZF-cGuiByHb35S9FA_z2Zrwhv7MjTFlEoMWbpPl_7vXkF1FNAmBFk6y2B4bEWhXs_e7bGPK6Z0ool1oBFa-5NF3Uh4NL4s9veyUpq5SJ7QaVfMqYb1g5xTvwnMR-sHdF40WYf6vulivVvxxNoN1jQPL5E8MlPzdSHPwH2d3SXs8sYGZWksu3qYzQ_dtZK_1oBgepsroUdmdJ7LTz4GnRk5kjkwO28iKwAepEX0mVeNMos5j3g/p.png",
+      "https://link.storjshare.io/s/jvqr6ewpzhptfjsq5ntctchx2eqa/bucket0/portfolio/lms.png?wrap=0",
     source: "https://github.com/vishal-bhangare/LibraryManagementSystem",
     live: "https://github.com/vishal-bhangare/LibraryManagementSystem/blob/master/LibraryManagementSystem.jar",
   },
@@ -13,7 +13,7 @@ export const Projects = [
     description: "I have created this notes app using <u>HTML JS PHP</u>",
     date: "March 2023",
     image:
-      "https://uc06edf1a04071c226b1012ef964.previews.dropboxusercontent.com/p/thumb/AB7r--ZuCidO-zvU9jZ3V0jwKza3OrKSHNoDHKTPP9M_K2IMU6PwxBl5GF0WhDE-kcIvGoAPuwT6raUwZHX7UccCXqgEJXl7Jzf7TenK_mMUcigz8Ik2_1crhapXgAhfuZoViKnj_lov_qFVk6BeLXu62MizGOP6yRF2ZKcP-5P-46OrV5M54H5ZN5cF7GbsEh07GzsEztKp8gflUpVXX_Rk23-3i12gOz2pHnXCUcDDAU888hAmNO26AL7dBOzVR6pnHhkzoVK6uqdxTabEbsZ4FBdz8ROeQei8Vwwd7UPH8mQ0AIRgY1QJ8rk9VQH75nPlJJdOYyEtw7Zpe7Y4OEYJp--T85AWbMPr-q01a3Vjkb6yaSZ-4PPWH0NMsT3GUeqKZ95_397c_EOQb43Ojc0_pwn5oucG5_Re80dylc2AhA/p.png",
+      "https://link.storjshare.io/s/jvqr6ewpzhptfjsq5ntctchx2eqa/bucket0/portfolio/ams.png?wrap=0",
     source:
       "https://github.com/vishal-bhangare/Student-Attendance-Management-System",
     live: "http://project-ams.rf.gd/",
@@ -24,7 +24,7 @@ export const Projects = [
       "I have created this notes app using <u>MEAN</u> stack. In this version i have used express api to operate data while in the first version of this project i have used dummy JSON server to store data",
     date: "April 2023",
     image:
-      "https://uc2715ff1eb8476687eef570eac0.previews.dropboxusercontent.com/p/thumb/AB6QPvZuDABaxjDULHKmx8zZARlli5yTccdq1OJrQ_9UTCNnSvf5cbyqQIhMu3uPb2UVuZJ4X8mk2Z7ugivo9-ajfLWqfZ9lZgCfXasOANFfExb2e11H0trsHXhNfvRBcOimATYm1WqWSqnnjGHrkw_vayHf1rQmd7V9qt269N-DFg7boLoz0kz2hHORRPoViMxn8taKuJWwdv1F1BTP7uhdafDnMFskjZjpkhwr9Tv1Ac3ll2xtf5O6PWu7CJNTt2dNYIHe5p8_64A2XGwTtsjstNAvfoCpH3_-6OvfPIPvNHVivolATUtCwTLsEywYA42G_8048HJa0kmf72Ld5bloTMEuzqXDrSssyCOfNvpiDsiSebOCeOkHtIq-JyYRBpIKh653LUPOad-IZMLCJLvXnkUzI8SV4MpJovP4yWDiaA/p.png",
+      "https://link.storjshare.io/s/jvqr6ewpzhptfjsq5ntctchx2eqa/bucket0/portfolio/notev2.png?wrap=1",
     source: "https://github.com/vishal-bhangare/notesv2",
     live: "https://notesv2-by-vishal.web.app/",
   },
