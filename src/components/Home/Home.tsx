a41d383fe2f215a584d260570d3158694c6970bb
vishalBhangare <vishalbhangare2@gmail.com>
2023-12-06 20:12:51 +0530

    added animation in name

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import styles from "./Home.module.scss";
 import { HashLink as Link } from "react-router-hash-link";
 
@@ -7,15 +8,34 @@ interface Props {
 }
 
 const Home = ({ activeLink, onChange }: Props) => {
+  const nameCharsRefs = useRef<HTMLSpanElement[]>([]);
   function setActiveLink(name: string) {
     onChange(name);
   }
+  useEffect(() => {
+    const elements = nameCharsRefs.current;
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.animationDelay = i / 12 + "s";
+    }
+  }, [nameCharsRefs]);
   return (
     <>
       <div className={styles.container} id="home">
         <div className={styles.content}>
           <h1 className={styles.intro}>
-            Hello, I'm <span className={styles.name}>Vishal Bhangare</span>.
+            Hello, I'm{" "}
+            <div className={styles.name}>
+              {[..."Vishal Bhangare"].map((char, i) => (
+                <span
+                  key={i}
+                  ref={(el: HTMLSpanElement) => nameCharsRefs.current.push(el)}
+                  className={styles.char}
+                >
+                  {char}
+                </span>
+              ))}
+            </div>
+            .
           </h1>
           <p className={styles.description}>A Student and developer.</p>
           <Link
